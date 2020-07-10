7f8fcef2a0030886b473296a801c0bb6897842e0
YTMcGamer <ytmcgamer99@gmail.com>
2020-07-10 21:52:08 +0200

    Add files via upload

---
+++ TrueAim/Header Files/menu/menu.h
@@ -0,0 +1,12 @@
+//
+// love fn sucks
+// paulgamer stop pasteing plz
+// hehe cracked
+//
+// YTMcGamer#0131
+//
+#pragma once
+
+namespace Render {
+	bool Initialize();
+}
---
+++ TrueAim/Header Files/settings/settings.h
@@ -0,0 +1,14 @@
+//
+// love fn sucks
+// paulgamer stop pasteing plz
+// hehe cracked
+//
+// YTMcGamer#0131
+//
+#pragma once
+#include <wtypes.h>
+
+namespace SettingsHelper {
+	VOID Initialize();
+	VOID SaveSettings();
+}
