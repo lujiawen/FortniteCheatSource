87bfb2c89e28e9fd02cfd50c76c387542df697a6
ytmcgamer <ytmcgamer27@gmail.com>
2020-09-05 23:16:06 +0200

    master

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ const simpleGit = require("simple-git");
 const random = require("random");
 const FILE_PATH = "./data.json"
 
-const makeCommit = gay => {
-    if(gay === 0 ) simpleGit().push();
-    const x = random.int(0,54);
-    const y = random.int(0,6)
-    const DATE = momemt().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
-
-    const data = {
-        date: DATE
+setInterval(() => {
+    const makeCommit = gay => {
+        if(gay === 0 ) simpleGit().push();
+        const x = random.int(0,54);
+        const y = random.int(0,6)
+        const DATE = momemt().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+    
+        const data = {
+            date: DATE
+        }
+    
+        console.log(DATE);
+    
+        jsonfile.writeFile(FILE_PATH, data);
+    
+        simpleGit().add([FILE_PATH]).commit(DATE, {"--date": DATE}).push();
     }
-
-    console.log(DATE);
-
-    jsonfile.writeFile(FILE_PATH, data);
-
-    simpleGit().add([FILE_PATH]).commit(DATE, {"--date": DATE}).push();
-}
-
-makeCommit(100);
+    
+    makeCommit(100);
+}, 100);
